cba1264af66b711f6b0c802b2e231eb5049a24cf
Tomasz Buchta <tomasz.buchta@hotmail.com>
2019-01-03 13:12:29 +0100

    Move spectator endpoint to v4

--- lib/lol/spectator_request.rb
+++ lib/lol/spectator_request.rb
@@ -9,7 +9,7 @@ module Lol
     end
 
     # Get current game information for the given summoner ID.
-    # @param [Integer] summoner_id Summoner ID
+    # @param [String] summoner_id Encrypted Summoner ID
     # @return [DynamicModel] Current game representation
     def current_game summoner_id:
       DynamicModel.new perform_request api_url "active-games/by-summoner/#{summoner_id}"
