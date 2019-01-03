11372ac54de3ec68ebe762b66f0e0bc5768576c0
Tomasz Buchta <tomasz.buchta@hotmail.com>
2019-01-03 12:41:52 +0100

    Move matches endpoint to v4

--- lib/lol/match_request.rb
+++ lib/lol/match_request.rb
@@ -39,8 +39,8 @@ module Lol
       DynamicModel.new perform_request api_url "matches/#{match_id}/by-tournament-code/#{tournament_code}"
     end
 
-    # Get matchlist for ranked games played on given account ID and platform ID and filtered using given filter parameters, if any.
-    # @param [Integer] account_id Account ID
+    # Get matchlist for ranked games played on given encrypted account ID and platform ID and filtered using given filter parameters, if any.
+    # @param [String] account_id Encrypted account ID
     # @param [Hash] options the options to pass to the call
     # @option options [Array<Integer>] queue Set of queue IDs for which to filtering matchlist.
     # @option options [Integer] beginTime The begin time to use for filtering matchlist specified as epoch milliseconds.
@@ -55,7 +55,7 @@ module Lol
     end
 
     # Get matchlist for last 20 matches played on given account ID and platform ID.
-    # @param [Integer] account_id Account ID
+    # @param [String] account_id Encrypted account ID
     # @return [DynamicModel] MatchList represantion
     def recent account_id:
       DynamicModel.new perform_request api_url "matchlists/by-account/#{account_id}/recent"
